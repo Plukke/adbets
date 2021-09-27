5bc3c2dc5e7ac1b39fb57d82c8a094fd34f62ca2
Giovanny Bernal <giovas94@gmail.com>
2021-09-27 12:23:54 -0500

    minor changes

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,62 +1,108 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { ipfs, json, Address } from "@graphprotocol/graph-ts";
 import {
   PoolFactory,
   PoolCreated,
-  PoolUpdated
-} from "../generated/PoolFactory/PoolFactory"
-import { ExampleEntity } from "../generated/schema"
+  PoolUpdated,
+} from "../generated/PoolFactory/PoolFactory";
+import { Pool } from "../generated/templates/Pool/Pool";
+import { Pool as EntityPool, Category, Group } from "../generated/schema";
 
-export function handlePoolCreated(event: PoolCreated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+function handleCategory(
+  factoryContract: PoolFactory,
+  poolContract: Pool
+): void {
+  let categoryEntity = Category.load(poolContract.category().toString());
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  if (!categoryEntity) {
+    categoryEntity = new Category(poolContract.category().toString());
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    const categoryData = factoryContract.categories(poolContract.category());
+
+    categoryEntity.name = categoryData.value1;
+    categoryEntity.status = categoryData.value3;
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  categoryEntity.save();
+}
 
-  // Entity fields can be set based on event parameters
-  entity._address = event.params._address
-  entity.name = event.params.name
+function handleGroup(factoryContract: PoolFactory, poolContract: Pool): void {
+  let groupEntity = Group.load(poolContract.group().toString());
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+  if (!groupEntity) {
+    groupEntity = new Group(poolContract.group().toString());
 
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
+    const categoryData = factoryContract.groups(poolContract.group());
 
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract._categoryIds(...)
-  // - contract._groupIds(...)
-  // - contract._poolIds(...)
-  // - contract.activePools(...)
-  // - contract.categories(...)
-  // - contract.deployedPools(...)
-  // - contract.getActivePools(...)
-  // - contract.getBalance(...)
-  // - contract.getDeployedPools(...)
-  // - contract.groups(...)
-  // - contract.listCategories(...)
-  // - contract.listGroups(...)
+    groupEntity.category = categoryData.value1.toString();
+    groupEntity.name = categoryData.value2;
+    groupEntity.country = categoryData.value3;
+    groupEntity.status = categoryData.value5;
+  }
+
+  groupEntity.save();
+}
+
+export function handlePoolCreated(event: PoolCreated): void {
+  let entity = EntityPool.load(event.params._address.toHex());
+
+  if (!entity) {
+    entity = new EntityPool(event.params._address.toHex());
+    entity.count = BigInt.fromI32(0);
+  }
+
+  entity.count = entity.count.plus(BigInt.fromI32(1));
+
+  let factoryContract = PoolFactory.bind(
+    Address.fromString("0xCce024598962C74aD36337569321570C95d7e3d1")
+  );
+  let poolContract = Pool.bind(event.params._address);
+
+  entity.name = poolContract.name();
+  entity.startTimestamp = poolContract.startTimestamp();
+  entity.endTimestamp = poolContract.endTimestamp();
+  entity.acceptDraw = poolContract.acceptDraw();
+  entity.riskLevel = poolContract.riskLevel();
+  entity.url = poolContract.url();
+  entity.result = 0;
+  entity.status = poolContract.status();
+  entity.creator = poolContract.creator();
+
+  // check if category & group exists or needs to be created
+  handleCategory(factoryContract, poolContract);
+  handleGroup(factoryContract, poolContract);
+
+  entity.category = poolContract.category().toString();
+  entity.group = poolContract.group().toString();
+
+  // retrieve pool metadata
+  const hash = entity.url.replace("https://api.thegraph.com/ipfs/", "");
+  let data = ipfs.cat(hash);
+  if (!!data) {
+    let value = json.fromBytes(data);
+    if (!value.isNull()) {
+      let obj = value.toObject();
+      let description = obj.get("description");
+      let venue = obj.get("venue");
+      let homeLabel = obj.get("homeLabel");
+      let awayLabel = obj.get("awayLabel");
+
+      if (description) {
+        entity.description = description.toString();
+      }
+      if (venue) {
+        entity.venue = venue.toString();
+      }
+      if (homeLabel) {
+        entity.homeLabel = homeLabel.toString();
+      }
+      if (awayLabel) {
+        entity.awayLabel = awayLabel.toString();
+      }
+    }
+  }
+
+  entity.save();
 }
 
 export function handlePoolUpdated(event: PoolUpdated): void {}
